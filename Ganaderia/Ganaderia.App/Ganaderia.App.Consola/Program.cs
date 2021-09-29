db95ddf049cc6370b19778de0c29b8e9d2d0c85c
NohelyAleja <90666534+NohelyAleja@users.noreply.github.com>
2021-09-28 19:03:28 -0500

    V1.0.0 Creacion Inicial del proyecto

---
+++ Ganaderia/Ganaderia.App/Ganaderia.App.Consola/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Ganaderia.App.Dominio;
+using Ganaderia.App.Persistencia;
+
+namespace Ganaderia.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioGanadero _repositorioGanadero = new RepositorioGanadero(new Persistencia.AppContext()); 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AddGanadero();
+        }
+private static void AddGanadero()
+        {
+            var ganadero = new Ganadero
+            {
+                Nombres = "Sergio",
+                Apellidos = "Moreno",
+                NumeroTelefono = "3148596562",
+                Correo = "sergiomintic@mintic.edu.co",
+                Contraseña = "123456",
+                Latitud = 4554,
+                Longitud = 5454
+            };
+
+            _repositorioGanadero.AddGanadero(ganadero);
+
+
+        }
+        
+    }
+}
---
+++ Ganaderia/Ganaderia.App/Ganaderia.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,13 @@
+namespace Ganaderia.App.Dominio
+{
+    public class Persona
+    {
+        public int Id { get; set;}
+        public string Nombres { get; set;}
+        public string Apellidos { get; set;}
+        public string NumeroTelefono { get; set;}
+        public string Correo { get; set;}
+        public string Contraseña { get; set;}
+    }
+
+}
